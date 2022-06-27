bb066ded097bd78aadeedd3e4e30cd2f831182c9
Phyllis <936427540@qq.com>
2022-06-27 14:25:01 +0800

    delete utils

--- utils/temp.py
+++
@@ -1,5 +0,0 @@
-from data_utils import get_pos_vocabulary, get_stopwords, get_supported_pos_tags
-pos_vocabulary = get_pos_vocabulary()
-print(len(pos_vocabulary))
-stop_words = get_stopwords()
-supported_pos_tags = get_supported_pos_tags()
